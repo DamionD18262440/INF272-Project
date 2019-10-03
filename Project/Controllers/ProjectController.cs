8398485852c7857f1bb0be5e984f3054523950e3
Keagzdecas <50364749+Keagzdecas@users.noreply.github.com>
2019-10-03 10:53:59 +0200

    add files
    files

---
+++ Project/Controllers/ProjectController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult BudgetLimits()
+        {
+            return View();
+        }
+        public ActionResult SpendingTransaction()
+        {
+            return View();
+        }
+        public ActionResult IncomeTransaction()
+        {
+            return View();
+        }
+        public ActionResult InvestmentCalculator()
+        {
+            return View();
+        }
+        public ActionResult MonthlySpending()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult ReportNetworth()
+        {
+            return View();
+        }
+        public ActionResult ReportSavings()
+        {
+            return View();
+        }
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult UpdateUser()
+        {
+            return View();
+        }
+        public ActionResult InvestmentMade()
+        {
+            return View();
+        }
+    }
+}
