a43a6a4cd0f5a1108c2af7e2b70ad83066cb35ab
DamionD18262440 <50363420+DamionD18262440@users.noreply.github.com>
2019-10-17 17:14:19 +0200

    Investment Calculator Functionality and linking database

--- Project/Controllers/ProjectController.cs
+++ Project/Controllers/ProjectController.cs
@@ -29,6 +29,90 @@ namespace Project.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult InvestmentCalculator(double amount, double interestrate, int interesttype, int interestperiod, int investmenttype, double numofyears)
+        {
+            double Mamount = amount;
+            double Minterestrate = interestrate;
+            int Minteresttype = interesttype;
+            int Minterestperiod = interestperiod;
+            int Minvestmenttype = investmenttype;
+            double Mnumofyears = numofyears;
+
+            if (Minteresttype == 1)
+            {
+                if (Minterestperiod == 1)
+                {
+                    double brackets = (1 + (Minterestrate * 0.01));
+                    double exponent = numofyears * 1;
+                    double power = Math.Pow(brackets, exponent);
+
+                    double compoundinvestment = amount * power;
+
+                    ViewBag.Message = compoundinvestment;
+                }
+                else if (Minterestperiod == 2)
+                {
+                    double brackets = (1 + ((Minterestrate * 0.01) * 0.5));
+                    double exponent = numofyears * 2;
+                    double power = Math.Pow(brackets, exponent);
+
+                    double compoundinvestment = amount * power;
+
+                    ViewBag.Message = compoundinvestment;
+                }
+                else if (Minterestperiod == 3)
+                {
+                    double brackets = (1 + ((Minterestrate * 0.01) * 0.25));
+                    double exponent = numofyears * 4;
+                    double power = Math.Pow(brackets, exponent);
+
+                    double compoundinvestment = amount * power;
+
+                    ViewBag.Message = compoundinvestment;
+                }
+                else if (Minterestperiod == 4)
+                {
+                    double brackets = (1 + ((Minterestrate * 0.01) * 0.083333333));
+                    double exponent = numofyears * 12;
+                    double power = Math.Pow(brackets, exponent);
+
+                    double compoundinvestment = amount * power;
+
+                    ViewBag.Message = compoundinvestment;
+                }
+            }
+            else if (Minteresttype == 2)
+            {
+                if (Minterestperiod == 1)
+                {
+                    double finalinvestment = (amount * (1 + (Minterestrate / 100) * Mnumofyears));
+
+                    ViewBag.Message = finalinvestment;
+                }
+                else if (Minterestperiod == 2)
+                {
+                    double finalinvestment = (amount * (1 + ((Minterestrate * 0.01) * 0.5) * (Mnumofyears * 2)));
+
+                    ViewBag.Message = finalinvestment;
+                }
+                else if (Minterestperiod == 3)
+                {
+                    double finalinvestment = (amount * (1 + ((Minterestrate * 0.01) * 0.25) * (Mnumofyears * 4)));
+
+                    ViewBag.Message = finalinvestment;
+                }
+                else if (Minterestperiod == 4)
+                {
+                    double finalinvestment = (amount * (1 + ((Minterestrate * 0.01) * 0.08333) * (Mnumofyears * 12)));
+
+                    ViewBag.Message = finalinvestment;
+                }
+            }
+
+            return View();
+        }
         public ActionResult MonthlySpending()
         {
             return View();
